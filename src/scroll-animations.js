2479aeb818f4a1bda73ffcb18df3d41609da8cae
Kat <kmita.katarzyna.a@gmail.com>
2024-01-04 12:45:10 -0500

    fixing scroll-animations script

--- src/scroll-animations.js
+++ src/scroll-animations.js
@@ -5,9 +5,11 @@ const viewportWidth = window.innerWidth
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        if (entry.target.classList.contains('animate')) {
-            if (entry.isIntersecting) 
+        if (entry.target.classList.contains('animate') && !entry.target.classList.contains('animated')) {
+            if (entry.isIntersecting) {
                 entry.target.classList.add('animating');
+                entry.target.classList.add('animated');
+            }
             else return;
         } else return;
     });
